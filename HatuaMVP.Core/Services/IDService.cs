2bd60c56c1e81a83e904e2b838392d18575afaf1
DWAKEL\selorm <avokeselorm@gmail.com>
2019-02-01 17:47:51 +0000

    Id services created to hash database Id and prevent raw ID from behind exposed to user side

---
+++ HatuaMVP.Core/Services/IDService.cs
@@ -0,0 +1,35 @@
+﻿using HashidsNet;
+using System;
+using System.Linq;
+
+namespace HatuaMVP.Core.Services
+{
+    public static class IDService
+    {
+        private static readonly IHashids Hashids = new Hashids("B5i+$lb)B<uJkV'f-.zK}T4>U0#21", 10, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789");
+
+        public static string Encode(string prefix, long id)
+        {
+            var encoded = Hashids.EncodeLong(id);
+            if (string.IsNullOrWhiteSpace(encoded))
+            {
+                throw new Exception("Encoded ID should not be empty");
+            }
+
+            return $"{prefix}_{encoded}";
+        }
+
+        public static long? Decode(string encoded)
+        {
+            if (string.IsNullOrWhiteSpace(encoded))
+            {
+                return null;
+            }
+
+            var id = encoded.Split('_').Last();
+            var decoded = Hashids.DecodeLong(id).FirstOrDefault();
+
+            return decoded != 0 ? decoded : (long?)null;
+        }
+    }
+}
